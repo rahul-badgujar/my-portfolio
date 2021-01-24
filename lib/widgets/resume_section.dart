f568057c391ab0ea08880782ec3fe72928b00bd1
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-01-24 12:52:30 +0530

    added Pinch Zoom for resume

--- lib/widgets/resume_section.dart
+++ lib/widgets/resume_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinch_zoom_image_updated/pinch_zoom_image_updated.dart';
 
 import '../data.dart';
 
@@ -6,12 +7,16 @@ class ResumeSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxHeight: 1500,
-        ),
-        child: Image.asset(
-          data["resume"],
+      child: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: 1500,
+          ),
+          child: PinchZoomImage(
+            image: Image.asset(
+              data["resume"],
+            ),
+          ),
         ),
       ),
     );
