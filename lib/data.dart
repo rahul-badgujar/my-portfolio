f44625b78be3ba37feb061f9bbd87ddcbd2ca79e
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-01-31 10:34:09 +0530

    resume download fixed

--- lib/data.dart
+++ lib/data.dart
@@ -1,6 +1,8 @@
 final data = <String, dynamic>{
   "photo": "assets/images/dp.png",
   "resume-pdf": "assets/docs/resume.pdf",
+  "resume-download-url":
+      "https://github.com/rahul-badgujar/rahul-badgujar/raw/main/Resumes/RahulBadgujar_InternshalaResume.pdf",
   "linkedin-logo": "assets/icons/linkedin-icon.png",
   "github-logo": "assets/icons/github-icon.png",
   "codechef-logo": "assets/icons/contest-icon.png",
